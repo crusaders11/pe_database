c7bebf6b63b910770df51228ecdd00f24bb0ad64
Raghu Betina <raghu@firstdraft.com>
2018-05-30 21:43:51 +0000

    Whitelist columns in Deal for ActiveAdmin

--- app/admin/deals.rb
+++ app/admin/deals.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Deal do
+
+ permit_params :deal_name, :industry_id, :broker_id, :deal_lead, :ltm_revenue, :ltm_ebitda, :ioi_enterprise_value, :ioi_revenue_multiple, :ioi_ebitda_multiple, :loi_enterprise_value, :loi_revenue_multiple, :loi_ebitda_multiple, :final_enterprise_value, :final_revenue_multiple, :final_ebitda_multiple
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
