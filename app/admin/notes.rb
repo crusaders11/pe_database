28965b33aef7fe021a4d59188b1aeeb6b0aa1f64
Raghu Betina <raghu@firstdraft.com>
2018-05-30 21:44:06 +0000

    Whitelist columns in Note for ActiveAdmin

--- app/admin/notes.rb
+++ app/admin/notes.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Note do
+
+ permit_params :deal, :date, :notes, :deals_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
