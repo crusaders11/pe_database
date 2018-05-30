c1f253fa4e575326c9e3b9c8ea94ee365a181af4
Raghu Betina <raghu@firstdraft.com>
2018-05-30 21:44:01 +0000

    Whitelist columns in Broker for ActiveAdmin

--- app/admin/brokers.rb
+++ app/admin/brokers.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Broker do
+
+ permit_params :name, :company, :email, :phone
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
